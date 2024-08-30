ceeb6b48fa7c84d241bf1f775d65edbf624464a8
jayants924 <jayant@shorthills.ai>
2024-08-30 10:30:04 +0530

    Initial commit

---
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebDiaryAPI.Data;
+using WebDiaryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebDiaryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaryEntriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiaryEntriesController(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DiaryEntry>>> GetDiaryEntries()
+        {
+            return await _context.DiaryEntries.ToListAsync();
+        }
+    }
+}
